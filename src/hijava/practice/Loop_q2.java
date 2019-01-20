b973036323c38e8a03f8b9480625d727335428d3
dohyekim <dhleen5@hanmail.net>
2019-01-20 12:02:05 +0900

    111

--- src/hijava/practice/Loop_q2.java
+++ src/hijava/practice/Loop_q2.java
@@ -3,20 +3,25 @@ package hijava.practice;
 public class Loop_q2 {
 
 	public static void main(String[] args) {
-		for (int i = 2; i <= 100; i++) {
+		int total = 0;
+	
+		for (int num = 2; num <= 100; num++) {
 			boolean isPrime = true;
-			for (int j = 2; j <= (i-1); j++) {
-				if (i / j == 0)
+			for (int j = 2; j < num; j++) {
+				if (num % j == 0) {
 					isPrime = false; 
 				    break;
-				if (isPrime)
-					total += i;
-					
-					
-					
+				}
 			}
-	
+			
+			if (isPrime)
+					total += num;
+	         else
+	        	  System.out.println(num);
 
+		}
+		
+		System.out.println(total);
 	}
 
 }
